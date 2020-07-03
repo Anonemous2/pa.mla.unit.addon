b93524ed6abb02a50f61083dfb5b562b0d16562e
Anonemous2 <35852034+Anonemous2@users.noreply.github.com>
2020-07-03 10:23:00 -0400

    First release 0.1.0
    Added alot of stuff for the first public version

--- ui/mods/mla.unit.addon/shared_build.js
+++ ui/mods/mla.unit.addon/shared_build.js
@@ -5,8 +5,13 @@ var newBuild = {
 
     "/pa/units/addon/andreas/andreas.json": ["orbital", 9, { row: 2, column: 5 }],
 
+    "/pa/units/addon/stalker/stalker.json": ["vehicle", 9, { row: 1, column: 6 }],
+
+    "/pa/units/addon/rex/rex.json": ["bot", 9, { row: 2, column: 6 }],
+
     "/pa/units/addon/metal_generator/metal_generator.json": ["utility", 0, { row: 1, column: 5 }],
     "/pa/units/addon/adv_metal_generator/adv_metal_generator.json": ["utility", 0, { row: 0, column: 5 }],
+    "/pa/units/addon/solar_cell/solar_cell.json": ["utility", 0, { row: 2, column: 5 }],
 }
 if (Build && Build.HotkeyModel && Build.HotkeyModel.SpecIdToGridMap) {
     _.extend(Build.HotkeyModel.SpecIdToGridMap, newBuild);
